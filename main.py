1af1c94d87aa29c0c1c9287c14392846eac4d3b0
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-01-03 14:24:52 +0300

    moded speech

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ r = sr.Recognizer()
 def record_audio(ask=False):
     with sr.Microphone() as source:
         if ask:
-            print(ask)
+            samaritan_speak(ask)
         audio = r.listen(source)
         voice_data = ''
 
@@ -21,9 +21,9 @@ def record_audio(ask=False):
             voice_data = r.recognize_google(audio)
             print(voice_data)
         except sr.UnknownValueError:
-            print("Sorry, I did not get that")
+            samaritan_speak("Sorry, I did not get that")
         except sr.RequestError:
-            print("Sorry, my speech service is down")
+            samaritan_speak("Sorry, my speech service is down")
 
         return voice_data
 
@@ -40,29 +40,29 @@ def samaritan_speak(audio_string):
 
 def respond(voice_data):
     if 'what is your name' in voice_data:
-        print("I am Samaritan")
+        samaritan_speak("I am Samaritan")
 
     if 'what time is it' in voice_data:
-        print(ctime())
+        samaritan_speak(ctime())
 
     if 'search' in voice_data:
         search = record_audio('What do you want to search for?')
         url = 'https://google.com/search?q=' + search
         webbrowser.get().open(url)
-        print('Here is what I found for' + search)
+        samaritan_speak('Here is what I found for' + search)
 
     if 'find location' in voice_data:
         location = record_audio('What is the location?')
         url = 'https://google.nl/maps/place' + location + '/&amp;'
         webbrowser.get().open(url)
-        print('Here is the location of' + location)
+        samaritan_speak('Here is the location of' + location)
 
     if 'exit' in voice_data:
         exit()
 
 
 time.sleep(1)
-print("How can I help you?")
+samaritan_speak("How can I help you?")
 while 1:
     voice_data = record_audio()
     respond(voice_data)
